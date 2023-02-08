532a94b7dcf0c9f6f677d08b7e524db8c74afd8b
ashwini-mhatre <mashu97@gmail.com>
2023-02-08 12:42:23 +0530

    Add valid examples

--- plugins/filter/health_check_view.py
+++ plugins/filter/health_check_view.py
@@ -29,9 +29,8 @@ EXAMPLES = r"""
       actions:
         - name: health_check
           vars:
-            details: True
+            details: False
             checks:
-              - name: any_state_up
               - name: all_operational_state_up
               - name: min_operational_state_up
                 min_count: 1
@@ -54,16 +53,6 @@ EXAMPLES = r"""
 #             }
 #         },
 #         "all_operational_state_up": {
-#             "check_status": "successful",
-#             "interfaces_status_summery": {
-#                 "admin_down": 0,
-#                 "admin_up": 4,
-#                 "down": 0,
-#                 "total": 4,
-#                 "up": 4
-#             }
-#         },
-#         "any_state_up": {
 #             "check_status": "successful",
 #             "interfaces_status_summery": {
 #                 "admin_down": 0,
@@ -102,12 +91,256 @@ EXAMPLES = r"""
 # PLAY RECAP *********************************************************************
 # 10.0.150.231               : ok=5    changed=0    unreachable=0    failed=0    skipped=0    rescued=0    ignored=0   
 
+- name: Perform interfaces health checks with details as true
+  hosts: iosxr
+  gather_facts: false
+  tasks:
+  - name: INTERFACES Manager
+    ansible.builtin.include_role:
+      name: network.interfaces.run
+    vars:
+      actions:
+        - name: health_check
+          vars:
+            details: true
+            checks:
+              - name: all_operational_state_up
+                ignore_errors: true
+              - name: min_operational_state_up
+                min_count: 1
+                ignore_errors: true
+              - name: all_admin_state_up
+                ignore_errors: true
+              - name: min_admin_state_up
+                ignore_errors: true
+                min_count: 1
+
+# TASK [network.interfaces.run : INTERFACES health checks] *************************************************************
+# [WARNING]: Persistent connection logging is enabled for 10.0.150.115. This will log ALL interactions and WILL NOT
+# redact sensitive configuration like passwords. USE WITH CAUTION!
+# ok: [10.0.150.115] => {
+#     "failed_when_result": false,
+#     "health_checks": {
+#         "all_admin_state_up": {
+#             "check_status": "unsuccessful",
+#             "interfaces_status_summery": {
+#                 "admin_down": 4,
+#                 "admin_up": 4,
+#                 "down": 1,
+#                 "total": 8,
+#                 "up": 3
+#             }
+#         },
+#         "all_operational_state_up": {
+#             "check_status": "unsuccessful",
+#             "interfaces_status_summery": {
+#                 "admin_down": 4,
+#                 "admin_up": 4,
+#                 "down": 1,
+#                 "total": 8,
+#                 "up": 3
+#             }
+#         },
+#         "detailed_interface_status_summery": {
+#             "interfaces": {
+#                 "Bundle-Ether11": {
+#                     "admin": "up",
+#                     "name": "Bundle-Ether11",
+#                     "operational": "down,"
+#                 },
+#                 "GigabitEthernet0/0/0/0": {
+#                     "admin": "up",
+#                     "name": "GigabitEthernet0/0/0/0",
+#                     "operational": "up,"
+#                 },
+#                 "GigabitEthernet0/0/0/1": {
+#                     "admin": "down",
+#                     "name": "GigabitEthernet0/0/0/1",
+#                     "operational": "NA"
+#                 },
+#                 "GigabitEthernet0/0/0/2": {
+#                     "admin": "down",
+#                     "name": "GigabitEthernet0/0/0/2",
+#                     "operational": "NA"
+#                 },
+#                 "Loopback888": {
+#                     "admin": "down",
+#                     "name": "Loopback888",
+#                     "operational": "NA"
+#                 },
+#                 "Loopback999": {
+#                     "admin": "down",
+#                     "name": "Loopback999",
+#                     "operational": "NA"
+#                 },
+#                 "MgmtEth0/RP0/CPU0/0": {
+#                     "admin": "up",
+#                     "name": "MgmtEth0/RP0/CPU0/0",
+#                     "operational": "up,"
+#                 },
+#                 "Null0": {
+#                     "admin": "up",
+#                     "name": "Null0",
+#                     "operational": "up,"
+#                 }
+#             }
+#         },
+#         "min_admin_state_up": {
+#             "check_status": "successful",
+#             "interfaces_status_summery": {
+#                 "admin_down": 4,
+#                 "admin_up": 4,
+#                 "down": 1,
+#                 "total": 8,
+#                 "up": 3
+#             }
+#         },
+#         "min_operational_state_up": {
+#             "check_status": "successful",
+#             "interfaces_status_summery": {
+#                 "admin_down": 4,
+#                 "admin_up": 4,
+#                 "down": 1,
+#                 "total": 8,
+#                 "up": 3
+#             }
+#         },
+#         "status": "successful"
+#     }
+# }
+# 
+# PLAY RECAP ***********************************************************************************************************
+# 10.0.150.115               : ok=5    changed=0    unreachable=0    failed=0    skipped=0    rescued=0    ignored=0   
+# 
+# 
+
+- name: Perform interfaces health checks with ignore_errors as false
+  hosts: iosxr
+  gather_facts: false
+  tasks:
+  - name: INTERFACES Manager
+    ansible.builtin.include_role:
+      name: network.interfaces.run
+    vars:
+      actions:
+        - name: health_check
+          vars:
+            details: true
+            checks:
+              - name: all_operational_state_up
+                ignore_errors: false
+              - name: min_operational_state_up
+                min_count: 1
+                ignore_errors: true
+              - name: all_admin_state_up
+                ignore_errors: true
+              - name: min_admin_state_up
+                ignore_errors: true
+                min_count: 1
+
+# 
+# TASK [network.interfaces.run : INTERFACES health checks] *************************************************************
+# [WARNING]: Persistent connection logging is enabled for 10.0.150.115. This will log ALL interactions and WILL NOT
+# redact sensitive configuration like passwords. USE WITH CAUTION!
+# fatal: [10.0.150.115]: FAILED! => {
+#     "failed_when_result": true,
+#     "health_checks": {
+#         "all_admin_state_up": {
+#             "check_status": "unsuccessful",
+#             "interfaces_status_summery": {
+#                 "admin_down": 4,
+#                 "admin_up": 4,
+#                 "down": 1,
+#                 "total": 8,
+#                 "up": 3
+#             }
+#         },
+#         "all_operational_state_up": {
+#             "check_status": "unsuccessful",
+#             "interfaces_status_summery": {
+#                 "admin_down": 4,
+#                 "admin_up": 4,
+#                 "down": 1,
+#                 "total": 8,
+#                 "up": 3
+#             }
+#         },
+#         "detailed_interface_status_summery": {
+#             "interfaces": {
+#                 "Bundle-Ether11": {
+#                     "admin": "up",
+#                     "name": "Bundle-Ether11",
+#                     "operational": "down,"
+#                 },
+#                 "GigabitEthernet0/0/0/0": {
+#                     "admin": "up",
+#                     "name": "GigabitEthernet0/0/0/0",
+#                     "operational": "up,"
+#                 },
+#                 "GigabitEthernet0/0/0/1": {
+#                     "admin": "down",
+#                     "name": "GigabitEthernet0/0/0/1",
+#                     "operational": "NA"
+#                 },
+#                 "GigabitEthernet0/0/0/2": {
+#                     "admin": "down",
+#                     "name": "GigabitEthernet0/0/0/2",
+#                     "operational": "NA"
+#                 },
+#                 "Loopback888": {
+#                     "admin": "down",
+#                     "name": "Loopback888",
+#                     "operational": "NA"
+#                 },
+#                 "Loopback999": {
+#                     "admin": "down",
+#                     "name": "Loopback999",
+#                     "operational": "NA"
+#                 },
+#                 "MgmtEth0/RP0/CPU0/0": {
+#                     "admin": "up",
+#                     "name": "MgmtEth0/RP0/CPU0/0",
+#                     "operational": "up,"
+#                 },
+#                 "Null0": {
+#                     "admin": "up",
+#                     "name": "Null0",
+#                     "operational": "up,"
+#                 }
+#             }
+#         },
+#         "min_admin_state_up": {
+#             "check_status": "successful",
+#             "interfaces_status_summery": {
+#                 "admin_down": 4,
+#                 "admin_up": 4,
+#                 "down": 1,
+#                 "total": 8,
+#                 "up": 3
+#             }
+#         },
+#         "min_operational_state_up": {
+#             "check_status": "successful",
+#             "interfaces_status_summery": {
+#                 "admin_down": 4,
+#                 "admin_up": 4,
+#                 "down": 1,
+#                 "total": 8,
+#                 "up": 3
+#             }
+#         },
+#         "status": "unsuccessful"
+#     }
+# }
+# 
+# PLAY RECAP ***********************************************************************************************************
+# 10.0.150.115               : ok=4    changed=0    unreachable=0    failed=1    skipped=0    rescued=0    ignored=0   
 
 """
 
 RETURN = """
   health_checks:
-    description:INTERFACES health checks 
+    description:INTERFACES health checks
     type: dict
 
 """
